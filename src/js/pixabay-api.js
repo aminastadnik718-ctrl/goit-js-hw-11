2934a006507b8dc82fd977315fea1fd5255554ac
amina1108 <aminastadnik963@gmail.com>
2026-05-19 12:10:06 +0200

    finish pixabay

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'API_KEY';
+const API_KEY = 'YOUR_API_KEY';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function getImagesByQuery(query) {
